f9f0691780d9aa0354f489cdd336ad87ae51c322
Simon Flachs <simon-flachs@hotmail.com>
2014-09-27 06:51:10 +0200

    added .gitignore and replaced config.php with an example

--- includes/configExample.php
+++ includes/configExample.php
@@ -1,17 +1,17 @@
 <?php
 
 // Publizon information
-	define("licenseKey","fdc226ef-e1ca-4379-b7b2-bf0b51847328");
+	define("licenseKey","1234");
 	define("soapServer","http://service.pubhub.dk/Retailer/V15/MediaService.asmx?WSDL");
-	define("login","bogertilborn@outlook.com");
-	define("password","Sneb8r");
+	define("login","abcd@abcd.com");
+	define("password","abcd");
 
 // Database information
 	define("DB_HOST","localhost");
 	define("DB_NAME","shopify_app");
 	define("DB_PORT","8889"); // default: 3306
-	define("DB_USER","root");
-	define("DB_PASS","root");
+	define("DB_USER","abcd");
+	define("DB_PASS","abcd");
 	
 
 	
